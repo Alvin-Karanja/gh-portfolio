acdbcd06b39143a7b24573770700e9dda8e9c120
Alvin-Karanja <alvin2@protonmail.com>
2022-07-19 17:52:58 +0300

    generated repositories component

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,9 @@
 export interface Repository {
+  name: string;
+  html_url: string;
+  description: string;
+  fork: boolean;
+  stargazers_count: number;
+  language: string;
+  forks_count: string;
 }
